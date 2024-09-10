c268eb1180648ac19e42472c9e147b0b245b24ba
Binod Kafle <mebinod50@gmail.com>
2024-09-10 08:53:40 +0545

    Update lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,68 +5,6 @@ return {
     config = true,
   },
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-buffer",           -- source for text in buffer
-      "hrsh7th/cmp-path",             -- source for file system paths
-      "L3MON4D3/LuaSnip",             -- snippet engine
-      "saadparwaiz1/cmp_luasnip",     -- for autocompletion
-      "rafamadriz/friendly-snippets", -- useful snippets
-      "onsails/lspkind-nvim",         -- adds icons to completion items
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
-
-      -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-      require("luasnip.loaders.from_vscode").lazy_load()
-      cmp.setup({
-        completion = {
-          completeopt = "menu,menuone,preview,noselect",
-        },
-        snippet = { -- configure how nvim-cmp interacts with snippet engine
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered()
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = 'select' }), -- previous suggestion
-          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = 'select' }), -- next suggestion
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-          ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        }),
-
-        -- sources for autocompletion
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- snippets
-          { name = "buffer" },  -- text within current buffer
-          { name = "path" },    -- file system paths
-        }),
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol_text',
-            menu = ({
-              buffer = "[Buffer]",
-              nvim_lsp = "[LSP]",
-              luasnip = "[Snippet]",
-              path = "[Path]",
-            }),
-          }),
-        },
-      })
-    end,
-  },
-  {
     'neovim/nvim-lspconfig',
     cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
     event = { 'BufReadPre', 'BufNewFile' },
@@ -79,10 +17,13 @@ return {
           local null_ls = require("null-ls")
           null_ls.setup({
             sources = {
-              null_ls.builtins.diagnostics.eslint,
+              null_ls.builtins.diagnostics.eslint_d,
               null_ls.builtins.formatting.prettier,
               null_ls.builtins.code_actions.gitsigns,
-              null_ls.builtins.formatting.black.with({ extra_args = { "--fast" }}),
+              null_ls.builtins.diagnostics.mdl, -- A tool to check Markdown files and flag style issues.
+              null_ls.builtins.formatting.isort,
+              null_ls.builtins.diagnostics.mypy,
+              null_ls.builtins.diagnostics.ruff,
             },
           })
         end,
@@ -115,7 +56,7 @@ return {
           -- override defaults from lsp_zero
           vim.keymap.set("n", "<leader>ca", '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
           -- code formatting
-          vim.keymap.set("n", "<leader>mp", function()
+          vim.keymap.set("n", "<leader>gf", function()
             vim.lsp.buf.format({})
           end)
         end
@@ -137,7 +78,8 @@ return {
           "tsserver",
           "html",
           "tailwindcss",
-          "pyright"
+          "pyright",
+          "ruff",
         },
         handlers = {
           default_setup,
@@ -156,6 +98,13 @@ return {
                   },
                 },
               },
+            })
+          end,
+          -- Setup for Pyright with specific filetypes
+          pyright = function()
+            require('lspconfig').pyright.setup({
+              filetypes = { "python" }, -- Restrict Pyright to Python files only
+              capabilities = lsp_capabilities,
             })
           end,
         },
